814ae5df68853c6648a788edb6c83a859ac13005
adamnano <32574741+adamnano@users.noreply.github.com>
2021-06-17 07:52:50 +0200

    Update final-version.R

--- final-version.R
+++ final-version.R
@@ -216,6 +216,16 @@ one_word_cloud <- wordcloud(words = d$word, freq = d$freq, min.freq = 1,
                             colors=brewer.pal(8, "Dark2"), scale = c(3, 0.2))
 
 
+#大學學歷
+get_least_degree <- function(Job_info, no_specify = TRUE)
+{
+  Job_degree <- Job_info %>% select(學歷要求)
+  if (no_specify == FALSE) Job_degree <- Job_degree %>% filter(學歷要求 != "不拘")
+  required_degree <- sapply(Job_degree[[1]], function(x){return(strsplit(x, split = "、")[1])}) %>% sapply(function(x){return(x[1] %>% str_trim())}) %>% unlist() %>% unname()
+  return(required_degree)
+}
+
+
 #工作內容
 library(readr)
 Job_content1 <- read_csv("經營_人資類content.csv")
